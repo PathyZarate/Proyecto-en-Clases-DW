aee772e158546ae7a1c072863e6ff2a6ac4d61bc
PathyZarate <patriciasoledadzaratearamayo@gmail.com>
2024-08-27 19:03:42 -0300

    leve cambio

--- Proyecto Codigo/JS/formulario.js
+++ Proyecto Codigo/JS/formulario.js
@@ -4,14 +4,14 @@ const newDateInput = document.getElementById('selected-date');
 const newTimeInput = document.getElementById('time-input');
 const newMotivoInput = document.getElementById('am-pm-selector');
 const addTaskButton = document.getElementById('addTask');
-
+//Funcion de añadir tarea
 function addTask() {
     const text = newTaskInput.value.trim();
     const desc = newDescInput.value.trim();
     const date = newDateInput.textContent.trim();
     const time = newTimeInput.value.trim();
     const motivo = newMotivoInput.value.trim();
-    
+    //validacion de que los campos esten llenos
     if (text && desc && date && time && motivo) {
         let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
         tasks.push({ text, desc, date, time, motivo, completed: false });
